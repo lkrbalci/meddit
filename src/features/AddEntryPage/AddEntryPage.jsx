b594298d33edbf78970d6506d12539fe0e31ab21
lkrbalci <65963431+lkrbalci@users.noreply.github.com>
2021-02-21 02:10:29 +0300

    ongoing UI

--- src/features/AddEntryPage/AddEntryPage.jsx
+++ src/features/AddEntryPage/AddEntryPage.jsx
@@ -6,26 +6,41 @@ import { MdOndemandVideo, MdInsertLink, MdChat } from "react-icons/md";
 
 const AddEntryPage = () => {
   const editorOptions = {
-    height: "500px",
+    height: "300px",
+    placeholder: "Article text here",
   };
 
   return (
     <div className={styles.addEntryPage}>
-      <div className={styles.buttonContainer}>
-        <button className={styles.button} id={styles.postButton}>
-          <MdChat />
-          Post
+      <div className={styles.title}>
+        <h2>Create a post</h2>
+      </div>
+      <div className={styles.postContainer}>
+        <div className={styles.buttonContainer}>
+          <button className={styles.button} id={styles.postButton}>
+            <MdChat />
+            Article
+          </button>
+          <button className={styles.button} id={styles.mediaButton}>
+            <MdOndemandVideo />
+            Media
+          </button>
+          <button className={styles.button} id={styles.linkButton}>
+            <MdInsertLink />
+            Link
+          </button>
+        </div>
+        <input type="text" placeholder="Title" id={styles.postTitleInput} />
+        <div id={styles.wysiwyg}>
+          <SunEditor setOptions={editorOptions} />
+        </div>
+        <button className={styles.postButton} id={styles.cancelPostButton}>
+          CANCEL
         </button>
-        <button className={styles.button} id={styles.mediaButton}>
-          <MdOndemandVideo />
-          Media
-        </button>
-        <button className={styles.button} id={styles.linkButton}>
-          <MdInsertLink />
-          Link
+        <button className={styles.postButton} id={styles.savePostButton}>
+          POST
         </button>
       </div>
-      <SunEditor setOptions={editorOptions} />
     </div>
   );
 };
